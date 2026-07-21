05d06aa65fdc80c12c219ad07a4ea6cd4cf1e1f6
Jay Prajapati <jayprajapati98006@gmail.com>
2026-07-21 20:48:50 +0530

    .......

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -1,19 +1,19 @@
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 import { initializeApp } from "firebase/app";
-import {getAuth, GoogleAuthProvider} from "firebase/auth"
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "interviewiq-ba6ba.firebaseapp.com",
-  projectId: "interviewiq-ba6ba",
-  storageBucket: "interviewiq-ba6ba.firebasestorage.app",
-  messagingSenderId: "862159592601",
-  appId: "1:862159592601:web:7308d702cd708076ddec08"
+  authDomain: "ai---interview.firebaseapp.com",
+  projectId: "ai---interview",
+  storageBucket: "ai---interview.firebasestorage.app",
+  messagingSenderId: "115528310676",
+  appId: "1:115528310676:web:4f515d064402b283123981",
 };
 
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 
-const provider = new GoogleAuthProvider()
+const provider = new GoogleAuthProvider();
 
-export {auth , provider}
+export { auth, provider };
